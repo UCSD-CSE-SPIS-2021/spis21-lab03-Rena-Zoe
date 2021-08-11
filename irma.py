f79efd197be2050722329f937a7ade270b31a221
zoeludena <88351041+zoeludena@users.noreply.github.com>
2021-08-11 20:31:53 +0000

    Hannah helped us fix the funkiest bug

--- irma.py
+++ irma.py
@@ -60,14 +60,58 @@ def irma():
         #(https://docs.python.org/3/library/functions.html#next)
         # pointreader is an iterator
 
+        t.penup()
+        #t.setpos(-17.66,0)
+        #t.setpos(-90, 45)
+       #t.setpos(-53.83, 22.5)
+
         for row in pointreader:
             # row is a list representing each line in the csv file
+
+            #We want to change the pen color and size to represent the different categories of a huricane based off it's wind speed (MPH)
+
+            if float(row[4]) <= 73:
+              t.pencolor("white")
+              t.pensize(2)
+              t.write("0", font=("Arial", 10, "normal"))
+            elif float(row[4]) >= 74 and float(row[4]) <= 95:
+              t.pencolor("blue")
+              t.pensize(4)
+              t.write("1", font=("Arial", 10, "normal"))
+            elif float(row[4]) >= 96 and float(row[4]) <= 110:
+              t.pencolor("green")
+              t.pensize(6)
+              t.write("2", font=("Arial", 10, "normal"))
+            elif float(row[4]) >= 111 and float(row[4]) <= 129:
+              t.pencolor("yellow")
+              t.pensize(8)
+              t.write("3", font=("Arial", 10, "normal"))
+            elif float(row[4]) >= 130 and float(row[4]) <= 156:
+              t.pencolor("orange")
+              t.pensize(10)
+              t.write("4", font=("Arial", 10, "normal"))
+            elif float(row[4]) >= 157:
+              t.pencolor("red")
+              t.pensize(12)
+              t.write("5", font=("Arial", 10, "normal"))
+            
+        #for i in row[2] and row[3]:
+              
+            t.setpos(float(row[3]), float(row[2]))
+            t.pendown()
+
+            #t.write("3", font=("Arial", 10, "normal"))
+
+            #We want to make a loop so that it will continue to draw out row 2 and row 3, creating a line
+            
+
             # Each comma separated element is in its own index position
             # This code just prints out the date and time elements of each
             # row in the file.
             # Make sure you understand what is happening here.
             # Then, you'll need to change this code
             print("Date:", row[0], "Time:", row[1])
+
             #We know latitude is row 2 longtitude is row 3, wind is row 4
 
 
